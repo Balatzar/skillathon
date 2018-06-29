17a1a0b36e908874b54fe4fd568f8c07d68d8253
balatzar <bataprod@gmail.com>
2018-06-29 20:22:29 +0200

    Redirect from public pages if logged in

--- app/controllers/public_pages_controller.rb
+++ app/controllers/public_pages_controller.rb
@@ -1,2 +1,9 @@
 class PublicPagesController < ApplicationController
+  before_action :redirect_if_logged_in
+
+  private
+
+  def redirect_if_logged_in
+    redirect_to after_sign_in_path_for(current_user) if current_user
+  end
 end
