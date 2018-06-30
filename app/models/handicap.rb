c0fda23f4ce3a9dc194fc6899fa792a8a79b0d6b
balatzar <bataprod@gmail.com>
2018-06-30 11:39:38 +0200

    Fix variables order

--- app/models/handicap.rb
+++ app/models/handicap.rb
@@ -1,6 +1,10 @@
 class Handicap < ApplicationRecord
   belongs_to :user
-  has_many :variables, inverse_of: :handicap, dependent: :destroy
+  has_many :variables,
+    -> { order "created_at asc" },
+    inverse_of: :handicap,
+    dependent: :destroy
+
   accepts_nested_attributes_for :variables, reject_if: :all_blank, allow_destroy: true
 
   validates :description, presence: true
